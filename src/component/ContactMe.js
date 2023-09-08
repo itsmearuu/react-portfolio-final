9616e883976dcef382b6635dff77ff5dfe7e7232
itsmearuu <mirandajohnlesther@gmail.com>
2023-09-08 08:01:23 +0800

    gh-pages

--- src/component/ContactMe.js
+++ src/component/ContactMe.js
@@ -6,7 +6,6 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import { faFacebook } from '@fortawesome/free-brands-svg-icons';
 import { faInstagram } from '@fortawesome/free-brands-svg-icons';
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 const ContactMe = () => {
     return ( 
@@ -14,11 +13,12 @@ const ContactMe = () => {
             <div className="container text-center my-5 py-5">
                 <div className="socials">
                     <h1 className='text-white'>Feel Free to Conact Me:</h1>
-
-                    <a href="https://github.com/itsmearuu"><FontAwesomeIcon className='text-white p-2' icon={faGithub} size='3x' /></a>
-                    <a href="https://www.facebook.com/Aleruzregtm1/"><FontAwesomeIcon className='text-white p-2' icon={faFacebook} size='3x'/></a>
-                    <a href="https://www.instagram.com/jl_aru/"><FontAwesomeIcon className='text-white p-2' icon={faInstagram} size='3x'/></a>
-                    <a href=""><FontAwesomeIcon className='text-white p-2' icon={faEnvelope} size='3x'/></a>
+                    <div className='my-3 py-3'>
+                    <a href="https://github.com/itsmearuu"><FontAwesomeIcon className='text-white p-3' icon={faGithub} size='3x' /></a>
+                    <a href="https://www.facebook.com/Aleruzregtm1/"><FontAwesomeIcon className='text-white p-3' icon={faFacebook} size='3x'/></a>
+                    <a href="https://www.instagram.com/jl_aru/"><FontAwesomeIcon className='text-white p-3' icon={faInstagram} size='3x'/></a>
+                    </div>
+                    
                     <div className='container my-5 py-5'>
                         <h1 className='text-white'>Email me:</h1>
                         <Form>
